c620a39ad43f451415e5264d6b88de4b22a206ec
Aurélien Izoulet <32246187+aurelienizl@users.noreply.github.com>
2025-01-15 13:00:05 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,19 +2,6 @@
 
 set -e
 
-if [ "$EUID" -ne 0 ]; then
-   echo "Please run as root"
-   exit 1
-fi
-
-# Check python3
-if [ -z "$(which python3)" ]; then
-   echo "Please install python3"
-   exit 1
-fi
-
-sudo apt install curl python3-venv
-
 # Create and activate virtual environment
 python3 -m venv venv
 source venv/bin/activate
